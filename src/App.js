a88cedb49c066e7dcd1118ef8f8765b34d9538c2
Nora Moser <noralmoser@gmail.com>
2025-05-03 14:40:38 -0700

    5/3 practice

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,9 +7,26 @@ const messages = [
 ];
 
 function App() {
-  const step = 3
+  const [ step, setStep ] = useState(3);
+  const [ isOpen, setIsOpen ] = useState(true)
+
+  function changeStep(direction) {
+    setStep(prevStep => {
+
+      if((prevStep <= 3 && prevStep >= 2) && direction === "Previous") {
+        return prevStep - 1
+      } else if ((prevStep <= 2 && prevStep >= 1) && direction === "Next") {
+        return prevStep + 1
+      }
+
+      return prevStep
+    })
+  }
 
   return (
+    <>
+    <button className="close" onClick={() => setIsOpen(prevOpen => !prevOpen)}>&times;</button>
+    {isOpen && (
     <div className="steps">
         <div className="numbers">
           <div className={step === 1 ? "active" : ""}>1</div>
@@ -18,10 +37,13 @@ function App() {
         <p className="message">Step {step}: {messages[step -1]}</p>
 
         <div className="buttons">
-          <button style={{ backgroundColor: "#7950f2", color:"#fff"}}>Previous</button>
-          <button style={{ backgroundColor: "#7950f2", color:"#fff"}}>Next</button>
+          <button style={{ backgroundColor: "#7950f2", color:"#fff"}} onClick={() => changeStep('Previous')}>Previous</button>
+          <button style={{ backgroundColor: "#7950f2", color:"#fff"}} onClick={() => changeStep('Next')}>Next</button>
         </div>
     </div>
+    )
+    }
+    </>
   );
 }
 
